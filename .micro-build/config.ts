2e1f93679f56cd240f28f698381f28a8bcf7a7d9
GongT <gongteng524702837@gmail.com>
2016-12-27 19:10:57 +0800

    copied config files

--- .micro-build/config.ts
+++ .micro-build/config.ts
@@ -17,24 +17,24 @@ build.domainName(projectName + '.gongt');
 
 build.noDataCopy();
 build.systemInstallMethod('apk');
-build.systemInstall('python', 'py-pip', 'libsodium');
+build.systemInstall('python', 'py-pip', 'libsodium', 'privoxy');
 
+const fast_open = new_kernel();
 if (JsonEnv && JsonEnv.gfw) {
 	build.isInChina(JsonEnv.gfw.isInChina);
 	
-	const fast_open = new_kernel();
 	require('fs').writeFileSync(
 		require('path').resolve(__dirname, '../config.json'),
 		JSON.stringify(Object.assign({fast_open}, JsonEnv.gfw.shadowsocks), null, 8)
 	);
-	build.startupCommand('/usr/bin/sslocal', '--pid-file', '/var/run/ss-client.pid', '--fast-open', '-c', '/data/config.json');
+	build.startupCommand('/usr/sbin/privoxy /etc/privoxy/config ; /usr/bin/sslocal --pid-file /var/run/ss-client.pid -c /data/config.json');
 } else {
-	build.startupCommand('/usr/bin/sslocal', '--pid-file', '/var/run/ss-client.pid', '-q', '--fast-open');
+	build.startupCommand(`/usr/sbin/privoxy /etc/privoxy/config ; /usr/bin/sslocal --pid-file /var/run/ss-client.pid ${fast_open? '--fast-open' : ''}`);
 }
-build.shellCommand('/usr/bin/python');
+build.shellCommand('/bin/sh', '-c');
 // build.stopCommand('stop.sh');
 
-build.forwardPort(7070, 'tcp').publish(7070);
+build.forwardPort(8118, 'tcp').publish(7070);
 build.forwardPort(7070, 'udp').publish(7070);
 
 build.disablePlugin(EPlugins.jenv);
